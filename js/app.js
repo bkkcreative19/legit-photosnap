9991cf6ddc534a7d9a948d37df4e211c43071807
Baron King <bkkcreative17@gmail.com>
2020-09-29 23:13:10 -0500

    fixed mobile-nav

--- js/app.js
+++ js/app.js
@@ -4,6 +4,21 @@ const hamburger = document.querySelector(".hamburger");
 const navList = document.querySelector(".nav-list");
 const navBtn = document.querySelector(".nav-btn");
 
+const hamburger = document.querySelector(".hamburger");
+const dropDown = document.querySelector(".dropdown-nav");
+const navBtn = document.querySelector(".nav-btn");
+const ul = document.querySelector(".ul");
+
+hamburger.addEventListener("click", () => {
+  dropDown.classList.toggle("display");
+
+  if (dropDown.classList.contains("display")) {
+    dropDown.style.display = "flex";
+  } else {
+    dropDown.style.display = "none";
+  }
+});
+
 toggle.addEventListener("change", () => {
   cards.classList.toggle("show-monthly");
 });
